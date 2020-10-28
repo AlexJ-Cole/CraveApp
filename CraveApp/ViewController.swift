557f8c179c8c787d1ce25b54aa78831dc92a5417
AlexJ-Cole <alexcole531@gmail.com>
2020-10-28 11:15:11 -0700

    Initial Commit

---
+++ CraveApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CraveApp
+//
+//  Created by Alex Cole on 10/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
